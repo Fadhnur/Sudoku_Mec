a09ed629f990d88b2eda0e082ef2ad7b16612cf9
indragans <134286242+indragans@users.noreply.github.com>
2023-09-29 17:16:04 +0700

    Update Board.h

--- Board.h
+++ Board.h
@@ -1,34 +1,16 @@
 
 class Board{
 	private:
-		int cells [9][9]= {
-		{1,2,0,0,0,7,9,5,0},
-		{0,5,6,3,0,8,0,1,7},
-		{3,4,7,0,0,9,6,8,0},
-		{0,0,0,7,0,0,0,0,0},
-		{0,0,0,0,8,0,0,0,5},
-		{0,3,1,0,0,0,8,9,0},
-		{0,0,4,9,5,0,0,0,0},
-		{8,9,3,1,0,0,5,0,6},
-		{0,0,2,0,3,6,7,0,9}};
-
-		bool IsSudokuValid();
+		int cells [9][9];
 			
 	public:
 		Board();
-		int getCellatCoordinate (int baris, int kolom);	//getNumber
-        bool isCellAvailable (int baris, int kolom, int value);	//isMoveValid
-		void setCellatCoordinate (int baris, int kolom, int value);	//placeNumber
-
-		bool checkHorizontal (int index);	//
-		bool checkVertikal (int index);
-		bool checkRegional (int index);
-
-		bool IsSolved();
-		
-		bool IsValidMove(int baris, int kolom, int value);
-
-
-
-		void PrintBoard();
+		void initialization();
+		void setCellValue(int x, int y, int value);
+		void getCellValue(int x, int y);
+		bool isValidCell();
+		bool isFull();
+		bool checkVertical(int index);
+		bool checkHorizontal(int index);
+		bool checkRegion(int index);
 };
