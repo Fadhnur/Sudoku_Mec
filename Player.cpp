8e40bb9b8829c913ac7c38b2a6573c184f2abf01
Fad <124156063+Fadhnur@users.noreply.github.com>
2023-09-29 19:49:03 +0700

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -2,19 +2,16 @@
 #include "Player.h" //memanggil header
 using namespace std;
 
-string username();
-int winCount();
-
-Player :: Player(string uname, int WinCounter){
+void Player :: Player(string uname, int WinCounter){
     username = uname;
     WinCount = WinCounter;
 }
 
-void Player :: setName(string name){
+void Player :: setUsername(string name){
     username = name;
 }
 
-string Player ::getName(){
+string Player :: getUsername(){
     return username;
 }
 
